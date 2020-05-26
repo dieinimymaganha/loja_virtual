c43943c62e29a95853cff97e8ac81fdf718af870
Dieinimy <dieinimy@gmail.com>
2020-05-26 20:25:15 -0300

    Iniciando criação de login, utilizando Scopedmodel, talvez de pra usar isso no APP

--- loja_virtual/lib/main.dart
+++ loja_virtual/lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:loja_virtual/model/user_model.dart';
 import 'package:loja_virtual/screens/home_screen.dart';
 import 'package:loja_virtual/screens/login_screen.dart';
 import 'package:loja_virtual/screens/singup_screen.dart';
+import 'package:scoped_model/scoped_model.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,14 +12,15 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Flutter's Clothing",
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-          primarySwatch: Colors.blue,
-          primaryColor: Color.fromARGB(255, 4, 125, 141)),
-      home: HomeScreen(),
-    );
-
+    return ScopedModel<UserModel>(
+        model: UserModel(),
+        child: MaterialApp(
+          title: "Flutter's Clothing",
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+              primarySwatch: Colors.blue,
+              primaryColor: Color.fromARGB(255, 4, 125, 141)),
+          home: HomeScreen(),
+        ));
   }
 }
