75bf2420c56bc34d100ecc8d87e599922bfedcf8
Dieinimy <dieinimy@gmail.com>
2020-05-27 17:02:02 -0300

    Concluido logiin

--- loja_virtual/lib/model/user_model.dart
+++ loja_virtual/lib/model/user_model.dart
@@ -10,6 +10,13 @@ class UserModel extends Model {
 
   bool isLoading = false;
 
+
+  @override
+  void addListener(VoidCallback listener) {
+    super.addListener(listener);
+    _loadCurrentUser();
+  }
+
   void signUp(
       {@required Map<String, dynamic> userData,
       @required String pass,
@@ -35,12 +42,24 @@ class UserModel extends Model {
     });
   }
 
-  void singIn() async {
+  void singIn(
+      {@required String email,
+      @required String pass,
+      @required VoidCallback onSucess,
+      @required VoidCallback onFail}) async {
     isLoading = true;
     notifyListeners();
-    await Future.delayed(Duration(seconds: 3));
-    isLoading = false;
-    notifyListeners();
+    _auth.signInWithEmailAndPassword(email: email, password: pass).then((user) async {
+      firebaseUser = user;
+      await _loadCurrentUser();
+      onSucess();
+      isLoading = false;
+      notifyListeners();
+    }).catchError((e) {
+      onFail();
+      isLoading = false;
+      notifyListeners();
+    });
   }
 
   void recoverPass() {}
@@ -62,5 +81,19 @@ class UserModel extends Model {
         .collection('users')
         .document(firebaseUser.uid)
         .setData(userData);
+  }
+
+  Future<Null> _loadCurrentUser() async {
+    if (firebaseUser == null) firebaseUser = await _auth.currentUser();
+    if (firebaseUser != null) {
+      if (userData['name'] == null) {
+        DocumentSnapshot docUser = await Firestore.instance
+            .collection('users')
+            .document(firebaseUser.uid)
+            .get();
+        userData = docUser.data;
+      }
+    }
+    notifyListeners();
   }
 }
