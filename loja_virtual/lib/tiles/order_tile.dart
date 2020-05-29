6936d34c6844d0e7e84dd4b7024a49af265272f3
Dieinimy <dieinimy@gmail.com>
2020-05-29 17:59:22 -0300

    Finalizada tela do status do pedido

--- loja_virtual/lib/tiles/order_tile.dart
+++ loja_virtual/lib/tiles/order_tile.dart
@@ -16,14 +16,16 @@ class OrderTile extends StatelessWidget {
         child: Padding(
           padding: EdgeInsets.all(8.0),
           child: StreamBuilder<DocumentSnapshot>(
-              stream: Firestore.instance.collection("orders").document(orderId).snapshots(),
-              builder: (context, snapshot){
-                if(!snapshot.hasData)
+              stream: Firestore.instance
+                  .collection("orders")
+                  .document(orderId)
+                  .snapshots(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData)
                   return Center(
                     child: CircularProgressIndicator(),
                   );
                 else {
-
                   int status = snapshot.data["status"];
 
                   return Column(
@@ -33,31 +35,96 @@ class OrderTile extends StatelessWidget {
                         "Código do pedido: ${snapshot.data.documentID}",
                         style: TextStyle(fontWeight: FontWeight.bold),
                       ),
-                      SizedBox(height: 4.0,),
-                      Text(
-                          _buildProductsText(snapshot.data)
+                      SizedBox(
+                        height: 4.0,
                       ),
-                      SizedBox(height: 4.0,),
+                      Text(_buildProductsText(snapshot.data)),
+                      SizedBox(
+                        height: 4.0,
+                      ),
                       Text(
                         "Status do Pedido:",
                         style: TextStyle(fontWeight: FontWeight.bold),
                       ),
-                      SizedBox(height: 4.0,),
+                      SizedBox(
+                        height: 4.0,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          _buildCircle('1', 'Preparação', status, 1),
+                          Container(
+                            height: 1.0,
+                            width: 40.0,
+                            color: Colors.grey[500],
+                          ),
+                          _buildCircle('2', 'Transporte', status, 2),
+                          Container(
+                            height: 1.0,
+                            width: 40.0,
+                            color: Colors.grey[500],
+                          ),
+                          _buildCircle('3', 'Entrega', status, 3),
+                        ],
+                      )
                     ],
                   );
                 }
-              }
-          ),
-        )
-    );
+              }),
+        ));
   }
 
-  String _buildProductsText(DocumentSnapshot snapshot){
+  String _buildProductsText(DocumentSnapshot snapshot) {
     String text = "Descrição:\n";
-    for(LinkedHashMap p in snapshot.data["products"]){
-      text += "${p["quantity"]} x ${p["product"]["title"]} (R\$ ${p["product"]["price"].toStringAsFixed(2)})\n";
+    for (LinkedHashMap p in snapshot.data["products"]) {
+      text +=
+          "${p["quantity"]} x ${p["product"]["title"]} (R\$ ${p["product"]["price"].toStringAsFixed(2)})\n";
     }
     text += "Total: R\$ ${snapshot.data["totalPrice"].toStringAsFixed(2)}";
     return text;
+  }
+
+  Widget _buildCircle(
+      String title, String subtitle, int status, int thisStatus) {
+    Color backColor;
+    Widget child;
+
+    if (status < thisStatus) {
+      backColor = Colors.grey[500];
+      child = Text(
+        title,
+        style: TextStyle(color: Colors.white),
+      );
+    } else if (status == thisStatus) {
+      backColor = Colors.blue;
+      child = Stack(
+        alignment: Alignment.center,
+        children: <Widget>[
+          Text(
+            title,
+            style: TextStyle(color: Colors.white),
+          ),
+          CircularProgressIndicator(
+            valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+          ),
+        ],
+      );
+    } else {
+      backColor = Colors.green;
+      child = Icon(
+        Icons.check,
+        color: Colors.white,
+      );
+    }
+    return Column(
+      children: <Widget>[
+        CircleAvatar(
+          radius: 20.0,
+          backgroundColor: backColor,
+          child: child,
+        ),
+        Text(subtitle)
+      ],
+    );
   }
 }
